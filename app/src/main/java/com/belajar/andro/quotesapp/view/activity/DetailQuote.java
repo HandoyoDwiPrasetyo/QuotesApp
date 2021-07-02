158c245de180951f9eba1df13dafc758d961f751
Handoyo Dwi Prasetyo <HandoyoDwiPrasetyo@users.noreply.github.com>
2021-07-02 20:29:42 +0700

    Update Fiture
    Create fiture Share Quote in detail

--- app/src/main/java/com/belajar/andro/quotesapp/view/activity/DetailQuote.java
+++ app/src/main/java/com/belajar/andro/quotesapp/view/activity/DetailQuote.java
@@ -2,7 +2,11 @@ package com.belajar.andro.quotesapp.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.belajar.andro.quotesapp.R;
@@ -10,6 +14,7 @@ import com.belajar.andro.quotesapp.R;
 public class DetailQuote extends AppCompatActivity {
 
     private TextView tvQuote, tvAuthor, tvTag, tvDateAdd, tvDateModified;
+    private Button btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,17 @@ public class DetailQuote extends AppCompatActivity {
         tvTag = findViewById(R.id.detail_tag);
         tvDateAdd = findViewById(R.id.detail_dateadd);
         tvDateModified = findViewById(R.id.detail_datemodified);
+
+        btnShare = findViewById(R.id.share_quote);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, getIntent().getStringExtra("quote"));
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent,"Send To"));
+            }
+        });
 
         String contentquote = getIntent().getStringExtra("quote");
         String authorquote = getIntent().getStringExtra("author");
